bdf2300a8f7fafed3870bd357c06dd24627fb29b
Suyesh Bhandari <suyeshb@gmail.com>
2016-02-19 12:50:54 -0500

    base_price

--- app/models/base_price.rb
+++ app/models/base_price.rb
@@ -3,11 +3,15 @@ class BasePrice < ActiveRecord::Base
   belongs_to :supplier
 
   def gen_retail_price
-    @retail_price = RetailPrice.new
-    @retail_price.r_reg = supplier.base_price.b_reg + f_reg
-    @retail_price.r_mid = supplier.base_price.b_mid + f_mid
-    @retail_price.r_pre = supplier.base_price.b_pre + f_pre
-    @retail_price.retailer = retailer
-    @retail_price.save
+    supplier.retailers.all do |retailer|
+      retailer.retail_prices.all do |retail_price|
+        retailer.relations.where(supplier_id: supplier.id) do |relation|
+          retail_price.r_reg = b_reg + relation.f_reg
+          retail_price.r_mid = b_mid + relation.f_mid
+          retail_price.r_pre = b_pre + relation.f_pre
+          retail_price.save
+        end
+      end
+    end
   end
 end
